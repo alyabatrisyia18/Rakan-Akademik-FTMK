fcabd6dbc6b164bbf8c58c50a36d14fe51400b1a
ayunizahirah <n.ayzhrh@gmail.com>
2026-06-28 00:49:42 +0800

    update code

--- profile_edit.php
+++ profile_edit.php
@@ -218,7 +218,7 @@ if (isset($_POST['btnUpdate'])) {
 ?>
 
 <!DOCTYPE html>
-< lang="en">
+<html lang="en">
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
